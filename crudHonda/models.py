00add14ed2e647237c17c0f76ef4987e05d4b62e
Sakti <firmansyah.sakti14@gmail.com>
2020-10-08 14:29:41 +0700

    Adding image

--- crudHonda/models.py
+++ crudHonda/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ProductModel(models.Model):
     nama = models.CharField(max_length=30)
-    transmisi = models.CharField(max_length=6)
+    transmisi = models.CharField(max_length=9)
     warna = models.CharField(max_length=20)
     harga = models.IntegerField()
 
--- crudHonda/urls.py
+++ crudHonda/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,5 @@ urlpatterns = [
     path('Edit/<int:id>', views.editMotor, name="editMotor"),
     path('Update/<int:id>', views.updateProd, name="updateProd"),
     path('Delete/<int:id>', views.deleteProd, name="deleteProd")
-]
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
